27914ff6bf43659688e0bff0fffa3a89998e2a92
aaronzhang1234 <nimbyest@gmail.com>
2018-04-07 10:58:09 -0400

    added some more SQL injection prevention

--- sql inject/securelogin.php
+++ sql inject/securelogin.php
@@ -7,6 +7,11 @@
     $usern = $_POST['user'];
     $passw = $_POST['pass'];
 
+    //Prepared Statements.
+    echo "SQL prevention Using prepared Statements";
+    echo "<br>";
+    echo "User Name = $usern<br>";
+    echo "Password =  $passw<br>";
     $stmt = $conn->prepare("SELECT * FROM sqlinject WHERE uname = ? AND pword = ?");
     $stmt->bind_param("ss", $usern, $passw );
 
@@ -16,6 +21,24 @@
     if($num>0){
         echo "Successfully Logged In";
     }else{
-        echo "Failed to Login"
+        echo "Failed to Login";
     }
+    echo "<br><br>";
+
+    //Remove nonalphanumeric symbols
+    echo "SQL prevention removing all non alpha numeric statements";
+    echo "<br>";
+    $passw = preg_replace("/[^A-Za-z0-9]/", '', $passw);
+    $usern = preg_replace("/[^A-Za-z0-9]/", '', $usern);
+    echo "User Name = $usern<br>";
+    echo "Password =  $passw<br>";
+    $query = mysqli_query($conn, "SELECT * FROM sqlinject where uname = '$usern' AND pword = '$passw'"); 
+    $row = mysqli_fetch_array($query); 
+    if(!empty($row['uname']) AND !empty($row['pword'])){ 
+        echo "You Logged In"; 
+    }else{ 
+        echo "Failed to Login"; 
+    }
+
+    //Remove 
 ?>
